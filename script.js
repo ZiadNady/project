9ad249454230b7bd8c3ff05df7766bcf271fe1ba
ziad nady <z.nady@baytalebaa.com>
2024-09-03 16:50:09 +0300

    add text to full-screen-swiper-section

--- script.js
+++ script.js
@@ -9,22 +9,7 @@ var swiper = new Swiper(".full-screen-swiper .mySwiper", {
     scrollbar: {
         el: ".full-screen-swiper .swiper-scrollbar",
         hide: true,
-    },
-    on: {
-        slideChange: function () {
-            var videos = document.querySelectorAll('.video-slide');
-            videos.forEach(video => {
-                video.pause();
-            });
-
-            var activeSlide = swiper.slides[swiper.activeIndex];
-            var activeVideo = activeSlide.querySelector('.video-slide');
-
-            if (activeVideo) {
-                activeVideo.play();
-            }
-        }
-    }   
+    }, 
 });
 
 var swiper = new Swiper(".what_is_happening_section .mySwiper", {
